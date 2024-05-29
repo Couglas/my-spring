28e3b64828994370bf381052c18082e986f78a48
couglas <myfriend1997@outlook.com>
2024-05-30 00:03:47 +0800

    自定义scope

---
+++ bean-scope/src/main/java/bean/scope/ThreadLocalScopeDemo.java
@@ -0,0 +1,53 @@
+package bean.scope;
+
+import domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * 自定义scope示例
+ *
+ * @author couglas
+ * @since 2024/5/29
+ */
+public class ThreadLocalScopeDemo {
+    @Bean
+    @Scope(ThreadLocalScope.SCOPE_NAME)
+    public User user() {
+        return createUser();
+    }
+
+    private static User createUser() {
+        User user = new User();
+        user.setId(System.nanoTime());
+        return user;
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(ThreadLocalScopeDemo.class);
+
+        applicationContext.addBeanFactoryPostProcessor(beanFactory -> {
+            beanFactory.registerScope(ThreadLocalScope.SCOPE_NAME, new ThreadLocalScope());
+        });
+
+        applicationContext.refresh();
+
+        scopedBeansLookup(applicationContext);
+
+        applicationContext.close();
+    }
+
+    private static void scopedBeansLookup(AnnotationConfigApplicationContext applicationContext) {
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(() -> {
+                User user = applicationContext.getBean("user", User.class);
+                System.out.println(user);
+            });
+            thread.start();
+        }
+    }
+}
